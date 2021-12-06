0bb11b32e0930199a7219aa77c603037619a7a79
root <Yufei Chen>
2021-12-06 02:58:54 +0000

    Add poisoning attack on the PatchCamelyon dataset.

---
+++ poisoning_patchcamelyon.py
@@ -0,0 +1,62 @@
+import os
+import sys
+from attack.clean_label_attack import clean_label_attack
+os.environ["CUDA_VISIBLE_DEVICES"]= sys.argv[1]
+os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
+from attack.main_attack import PoisonAttack
+from attack.attack_utils import mia, check_mia, poison_attack
+
+import numpy as np
+import gc
+
+
+
+if __name__ == '__main__':
+    #for seed_amount in [40, 80, 200]:
+    INPUT_SIZE = (96, 96, 3)
+
+    poison_dataset_config = {
+        'dataset_name': 'patchcamelyon',
+        'input_shape': INPUT_SIZE,
+    }
+
+    attack_config = {
+        'iters': 1000,
+        'learning_rate': 0.01,
+        'batch_size': 100,
+        'if_selection': False
+    }
+
+    clean_label_flag = True 
+
+    if clean_label_flag:
+        clean_label_str = 'clean'
+    else:
+        clean_label_str = 'dirty'
+
+    for target_class in range(2):
+        for seed_amount in [1000]:
+            for encoder in ['inceptionv3', 'mobilenetv2', 'xception']:
+                for target_encoder in ['inceptionv3', 'mobilenetv2', 'xception']:
+                    if encoder == target_encoder:
+                        continue
+                    else:
+                        poison_config = {
+                            'poison_encoder_name': encoder,
+                            'poison_img_dir': './poisoning_dataset_{}_label/imgs/'.format(clean_label_str),
+                            'poison_label_dir': './poisoning_dataset_{}_label/labels/'.format(clean_label_str),
+                            'anchorpoint_img_dir': './poisoning_dataset_{}_label/anchorpoint_imgs/'.format(clean_label_str),
+                            'target_class': target_class,
+                            'seed_amount': seed_amount,
+                            'anchorpoint_amount': 1000,
+                            'clean_label_flag': clean_label_flag,
+                            'fcn_sizes': [128, 2],
+                            'transferable_attack_flag': False,
+                            'target_encoder_name': target_encoder,
+                        }
+                        poison_attack(poison_config=poison_config,
+                                      poison_dataset_config=poison_dataset_config,
+                                      attack_config=attack_config)
