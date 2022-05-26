48dcca9ed236e92f9675a525d6ae848f5e132e0c
Yufei Chen <yfchen.xjtu@gmail.com>
2022-05-26 20:42:05 +0800

    Update attack_example.sh
    Fix.

--- attack_example.sh
+++ attack_example.sh
@@ -1,8 +1,8 @@
 # Launching clean-label poisoning attack
 python poisoning_attack.py --target_class 0 --dataset cifar10 --encoder xception --seed_amount 1000 --attack_type clean_label
 # Inspecting poisoning attack
-python poisoning_attack.py --target_class 0 --dataset cifar10 --encoder xception --seed_amount 1000 --attack_type clean_label --check_mia False
+python poisoning_attack.py --target_class 0 --dataset cifar10 --encoder xception --seed_amount 1000 --attack_type clean_label --check_mia true
 # Launching dirty-label poisoning attack
 python poisoning_attack.py --target_class 0 --dataset cifar10 --encoder xception --seed_amount 1000 --attack_type dirty_label
 # Inspecting dirty-label poisoning attack
-python poisoning_attack.py --target_class 0 --dataset cifar10 --encoder xception --seed_amount 1000 --attack_type dirty_label --check_mia False
+python poisoning_attack.py --target_class 0 --dataset cifar10 --encoder xception --seed_amount 1000 --attack_type dirty_label --check_mia true
